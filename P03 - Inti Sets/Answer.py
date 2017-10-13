345dc067a9b632a0b9e212ef4a7fe37f61e2998d
meysam81 <MeysamAzad81@yahoo.com>
2017-10-13 10:59:47 +0330

    Revised, still hitting timeout

--- P03 - Inti Sets/Answer.py
+++ P03 - Inti Sets/Answer.py
@@ -1,4 +1,3 @@
-modulo = 1000000007
 def gcd(a, b):
     if a == 0:
         return b
@@ -12,11 +11,25 @@ def gcd(a, b):
         if a < b:
             a, b = b, a
         return gcd((a - b) / 2, b)
+def getPrime(n):
+    res = [1, n]
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            res.append(i)
+            if int(n / i) != i:
+                res.append(int(n / i))
+    return sorted(res)
+def primeRange(q, a, b):
+    res = []
+    for i in range(a, b + 1):
+        if gcd(i, q) == 1:
+            res.append(i)
+    return res
+modulo = 1000000007
 n = int(input())
 for i in range(n):
     q, a, b = list(map(int, input().strip().split()))
     s = 0
-    for j in range(a, b + 1):
-        if int(gcd(q, j)) == 1:
-            s += j
+    for j in primeRange(q, a, b):
+        s += j
     print(s % modulo)
